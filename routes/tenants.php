afa6d7d82cfec412189f3020f5b74f4d72f681c6
Alpha Olomi <alphaolomi@gmail.com>
2020-05-14 00:47:57 +0300

    Update tenants.php

--- routes/tenants.php
+++ routes/tenants.php
@@ -4,7 +4,24 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
 Route::get('users', function () {
     // todo add tenant views
     return App\Tenant\Models\User::all();
 });
+
+
+
+// Route::middleware('web')
+//     ->namespace('App\\Http\\Controllers\\Tenant\\')
+//     ->group(function () {
+//         Route::get('/', 'HomeController');
+//     });
