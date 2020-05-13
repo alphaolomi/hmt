82aec7e017add50b6791f5f8fae27237c1789b8d
Alpha Olomi <alphaolomi@gmail.com>
2020-05-14 01:14:16 +0300

    use web routes

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,16 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::domain('{tenant}.mumbo24.tech')->group(function () {
+
+    Route::get('/', function () {
+        return view('welcome');
+    });
+
+    Auth::routes();
+
+    Route::get('/home', 'HomeController@index')->name('home');
+
+});
