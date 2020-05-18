c07869196c085c5fb3fedf29ad73f40ae5c2540b
Alpha Olomi <alphaolomi@gmail.com>
2020-05-18 03:58:39 +0300

    Update server_deploy.sh

--- server_deploy.sh
+++ server_deploy.sh
@@ -19,7 +19,7 @@ echo "Deploying application ..."
     php artisan optimize
 
     # Reload PHP to update opcache
-    echo "" | sudo -S service php7.4-fpm reload
+    echo "" | sudo -S service php7.2-fpm reload
 # Exit maintenance mode
 php artisan up
 
