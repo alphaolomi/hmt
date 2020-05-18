8a10a6f79bb9b8f08bf39e49405cc8f398382f7f
Alpha Olomi <alphaolomi@gmail.com>
2020-05-18 03:42:28 +0300

    Create server_deploy.sh

---
+++ server_deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -e
+
+echo "Deploying application ..."
+
+# Enter maintanance mode
+(php artisan down --message 'The app is being (quickly!) updated. Please try again in a minute.') || true
+    # Update codebase
+    git fetch origin deploy
+    git reset --hard origin/deploy
+
+    # Install dependencies based on lock file
+    composer install --no-interaction --prefer-dist --optimize-autoloader
+
+    # Migrate database
+    php artisan migrate --force
+
+    # Clear cache
+    php artisan optimize
+
+    # Reload PHP to update opcache
+    echo "" | sudo -S service php7.4-fpm reload
+# Exit maintenance mode
+php artisan up
+
+echo "🚀 Application deployed!"
